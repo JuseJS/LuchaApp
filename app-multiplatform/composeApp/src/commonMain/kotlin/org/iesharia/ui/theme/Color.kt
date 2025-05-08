91b7879536d122f7a38a784549f989cb870a8bb6
Jose Antonio <joseanp0202@gmail.com>
2025-05-08 12:24:06 +0100

    feat: añadir color.kt con paleta de colores para lucha canaria
    Se ha creado el archivo color.kt que define los colores principales
    de la aplicación inspirados en la lucha canaria.
    
    La paleta está optimizada para pantallas OLED, aprovechando los negros
    puros y contrastes para mejorar la experiencia visual y la eficiencia
    energética del dispositivo.
    
    Los colores representan elementos tradicionales de este deporte canario
    como la arena, las camisetas de brega y el terrero.

---
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/ui/theme/Color.kt
@@ -0,0 +1,38 @@
+package org.iesharia.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// El negro puro para fondos
+val PureBlack = Color(0xFF000000)
+
+// Color arena
+val SandLight = Color(0xFFE6D2B5)
+val SandDark = Color(0xFFBFA580)
+
+// Azul canario
+val CanaryBlue = Color(0xFF1B4C84)
+val CanaryBlueLight = Color(0xFF3A78C9)
+
+// Rojo tradicional
+val TraditionalRed = Color(0xFFAA3333)
+val TraditionalRedLight = Color(0xFFD16666)
+
+// Verde laurisilva
+val LausisilvaGreen = Color(0xFF166B3D)
+val LaurisilvaGreenLight = Color(0xFF27A95F)
+
+// Colores para el tema oscuro
+val DarkBackground = PureBlack
+val DarkSurface = Color(0xFF121212)
+val DarkPrimary = SandLight
+val DarkSecondary = CanaryBlueLight
+val DarkTertiary = LaurisilvaGreenLight
+val DarkError = TraditionalRedLight
+
+// Colores para textos y contenido
+val DarkOnBackground = Color(0xFFF5F5F5)
+val DarkOnSurface = Color(0xFFE6E6E6)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkOnTertiary = Color(0xFF000000)
+val DarkOnError = Color(0xFF000000)
