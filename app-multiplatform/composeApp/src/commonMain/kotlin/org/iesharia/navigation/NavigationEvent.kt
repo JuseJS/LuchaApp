cf36cd286b5b6a78f06a16856cbaea78bb0685eb
Jose Antonio <joseanp0202@gmail.com>
2025-05-11 19:45:23 +0100

    feat(navigation): crear clase NavigationEvent para eventos de navegación
    Se define una clase sealed para representar eventos de navegación:
    - NavigateTo para navegar a una pantalla
    - NavigateBack para volver a la pantalla anterior

---
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/navigation/NavigationEvent.kt
@@ -0,0 +1,11 @@
+package org.iesharia.navigation
+
+import cafe.adriel.voyager.core.screen.Screen
+
+/**
+ * Eventos de navegación que pueden ser emitidos por los ViewModels
+ */
+sealed class NavigationEvent {
+    data class NavigateTo(val screen: Screen) : NavigationEvent()
+    data object NavigateBack : NavigationEvent()
+}
