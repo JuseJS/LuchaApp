b2a34b1eb5c50120d929822c3519bb213dac2026
Jose Antonio <joseanp0202@gmail.com>
2025-05-14 14:17:47 +0100

    refactor(common): traslada GetFavoritesUseCase al paquete features.common.domain.usecase
    Reubica el caso de uso para obtener favoritos al módulo common dentro de
    features para centralizar funcionalidad compartida entre diferentes características.

--- app-multiplatform/shared/src/commonMain/kotlin/org/iesharia/domain/usecase/GetFavoritesUseCase.kt
+++
@@ -1,8 +0,0 @@
-package org.iesharia.domain.usecase
-
-import org.iesharia.domain.model.Favorite
-import org.iesharia.domain.repository.CompetitionRepository
-
-class GetFavoritesUseCase(private val repository: CompetitionRepository) {
-    suspend operator fun invoke(): List<Favorite> = repository.getFavorites()
-}
