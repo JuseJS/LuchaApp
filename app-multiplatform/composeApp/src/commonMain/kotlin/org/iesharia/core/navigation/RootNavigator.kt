44230b8d19c71552b1bdff985552abdfc97c9e79
Jose Antonio <joseanp0202@gmail.com>
2025-05-14 14:00:12 +0100

    refactor(navigation): actualiza RootNavigator y sus importaciones
    Traslada el navegador principal al paquete core.navigation y actualiza la importación de LoginScreen para reflejar la nueva estructura.

--- app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/core/navigation/RootNavigator.kt
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/core/navigation/RootNavigator.kt
@@ -1,9 +1,9 @@
-package org.iesharia.navigation
+package org.iesharia.core.navigation
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
-import org.iesharia.ui.screens.login.LoginScreen
+import org.iesharia.features.auth.ui.screens.LoginScreen
 
 /**
  * Componente principal de navegación para la aplicación
