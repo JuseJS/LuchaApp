61fc7f411e89937e36983f69a2e756b734275696
Jose Antonio <joseanp0202@gmail.com>
2025-05-16 13:49:29 +0100

    feat: implementar rutas específicas para las pantallas de la aplicación

---
+++ app-multiplatform/shared/src/commonMain/kotlin/org/iesharia/core/navigation/Routes.kt
@@ -0,0 +1,78 @@
+package org.iesharia.core.navigation
+
+/**
+ * Objeto que contiene todas las rutas de navegación de la aplicación.
+ */
+object Routes {
+    /**
+     * Rutas relacionadas con la autenticación.
+     */
+    object Auth {
+        /**
+         * Ruta para la pantalla de inicio de sesión/registro.
+         */
+        object Login : NavigationRoute {
+            override val baseRoute: String = "auth/login"
+        }
+    }
+
+    /**
+     * Rutas relacionadas con la pantalla principal.
+     */
+    object Home {
+        /**
+         * Ruta para la pantalla principal.
+         */
+        object Main : NavigationRoute {
+            override val baseRoute: String = "home"
+        }
+    }
+
+    /**
+     * Rutas relacionadas con competiciones.
+     */
+    object Competition {
+        /**
+         * Ruta para la pantalla de detalle de competición.
+         */
+        data class Detail(private val competitionId: String? = null) : NavigationRouteWithParams<String> {
+            override val baseRoute: String = "competition/detail"
+            override val route: String
+                get() = if (competitionId != null) "$baseRoute/$competitionId" else "$baseRoute/{competitionId}"
+
+            override fun createRoute(params: String): String = "$baseRoute/$params"
+        }
+    }
+
+    /**
+     * Rutas relacionadas con equipos.
+     */
+    object Team {
+        /**
+         * Ruta para la pantalla de detalle de equipo.
+         */
+        data class Detail(private val teamId: String? = null) : NavigationRouteWithParams<String> {
+            override val baseRoute: String = "team/detail"
+            override val route: String
+                get() = if (teamId != null) "$baseRoute/$teamId" else "$baseRoute/{teamId}"
+
+            override fun createRoute(params: String): String = "$baseRoute/$params"
+        }
+    }
+
+    /**
+     * Rutas relacionadas con luchadores.
+     */
+    object Wrestler {
+        /**
+         * Ruta para la pantalla de detalle de luchador.
+         */
+        data class Detail(private val wrestlerId: String? = null) : NavigationRouteWithParams<String> {
+            override val baseRoute: String = "wrestler/detail"
+            override val route: String
+                get() = if (wrestlerId != null) "$baseRoute/$wrestlerId" else "$baseRoute/{wrestlerId}"
+
+            override fun createRoute(params: String): String = "$baseRoute/$params"
+        }
+    }
+}
