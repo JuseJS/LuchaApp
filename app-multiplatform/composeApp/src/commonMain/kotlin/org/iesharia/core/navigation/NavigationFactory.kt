e24f805d6941ddf645cb5eaeead357ffb5446eca
Jose Antonio <joseanp0202@gmail.com>
2025-05-16 13:49:46 +0100

    feat: crear factory para convertir rutas en pantallas de Voyager

---
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/core/navigation/NavigationFactory.kt
@@ -0,0 +1,54 @@
+package org.iesharia.core.navigation
+
+import cafe.adriel.voyager.core.screen.Screen
+import org.iesharia.features.auth.ui.screens.LoginScreen
+import org.iesharia.features.home.ui.screens.HomeScreen
+
+/**
+ * Factory que convierte rutas de navegación en pantallas de Voyager.
+ */
+class NavigationFactory {
+    /**
+     * Crea una pantalla de Voyager a partir de una ruta.
+     */
+    fun createScreen(navigationRoute: NavigationRoute): Screen {
+        return when (navigationRoute) {
+            is Routes.Auth.Login -> LoginScreen()
+            is Routes.Home.Main -> HomeScreen()
+
+            // Competiciones
+            is Routes.Competition.Detail -> {
+                val competitionId = getLastPathSegment(navigationRoute.route)
+                // Por ahora retornamos HomeScreen
+                // TODO: Implementar pantalla de detalle de competición
+                HomeScreen()
+            }
+
+            // Equipos
+            is Routes.Team.Detail -> {
+                val teamId = getLastPathSegment(navigationRoute.route)
+                // Por ahora retornamos HomeScreen
+                // TODO: Implementar pantalla de detalle de equipo
+                HomeScreen()
+            }
+
+            // Luchadores
+            is Routes.Wrestler.Detail -> {
+                val wrestlerId = getLastPathSegment(navigationRoute.route)
+                // Por ahora retornamos HomeScreen
+                // TODO: Implementar pantalla de detalle de luchador
+                HomeScreen()
+            }
+
+            // Ruta desconocida
+            else -> LoginScreen()
+        }
+    }
+
+    /**
+     * Obtiene el último segmento de una ruta, que normalmente es el parámetro.
+     */
+    private fun getLastPathSegment(route: String): String {
+        return route.split("/").last()
+    }
+}
