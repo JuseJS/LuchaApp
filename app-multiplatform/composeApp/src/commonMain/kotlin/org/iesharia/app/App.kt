0c6afd3e9308668f85e4b984ea985884819ee272
Jose Antonio <joseanp0202@gmail.com>
2025-05-17 22:17:07 +0100

    refactor(App): cambia LuchaAppTheme a WrestlingAppTheme

--- app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/app/App.kt
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/app/App.kt
@@ -2,14 +2,14 @@ package org.iesharia.app
 
 import androidx.compose.runtime.Composable
 import org.iesharia.core.navigation.RootNavigator
-import org.iesharia.core.ui.theme.LuchaAppTheme
+import org.iesharia.core.ui.theme.WrestlingAppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     org.koin.compose.KoinContext {
-        LuchaAppTheme {
+        WrestlingAppTheme {
             RootNavigator()
         }
     }
