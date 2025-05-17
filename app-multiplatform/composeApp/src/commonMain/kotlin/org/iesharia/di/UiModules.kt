a79b888a97ba640056415713b9083c70b78772d8
Jose Antonio <joseanp0202@gmail.com>
2025-05-17 19:29:18 +0100

    feat: registrar CompetitionDetailViewModel en el contenedor de dependencias

--- app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/di/UiModules.kt
+++ app-multiplatform/composeApp/src/commonMain/kotlin/org/iesharia/di/UiModules.kt
@@ -4,6 +4,7 @@ import org.iesharia.features.home.ui.viewmodel.HomeViewModel
 import org.iesharia.features.auth.ui.viewmodel.LoginViewModel
 import org.iesharia.core.navigation.NavigationFactory
 import org.iesharia.core.navigation.NavigationManager
+import org.iesharia.features.competitions.ui.viewmodel.CompetitionDetailViewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -11,6 +12,15 @@ import org.koin.dsl.module
 val viewModelModule = module {
     factory { HomeViewModel(get(), get(), get(), get(), get<NavigationManager>(), get()) }
     factory { LoginViewModel(get<NavigationManager>(), get()) }
+    factory { parameters ->
+        CompetitionDetailViewModel(
+            competitionId = parameters.get(),
+            competitionRepository = get(),
+            getFavoritesUseCase = get(),
+            navigationManager = get(),
+            errorHandler = get()
+        )
+    }
 }
 
 // Módulo para navegación UI
